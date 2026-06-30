b88fcf85fc60743faf945da011c7e5b420c9a781
Gautam <chawdagautam7@gmail.com>
2026-06-30 20:16:07 +0530

    Day 2: Learned variables, data types, scope, and user input

---
+++ var4.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    cout<<"Enter your age :";
+    cin>> a;
+    cout<<"you are "<<a<<" years old.";
+    return 0;
+}
---
+++ var5.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    string name;
+    cout<<"Enter your name:";
+    cin>>name;
+    int age;
+    cout<<"Enter your age:";
+    cin>>age;
+    float percentage;
+    cout<<"Enter your percentage:";
+    cin>>percentage;
+    char grade;
+    cout<<"enter your grade:";
+    cin>>grade;
+    cout<<"Name      :"<<name<<endl;
+    cout<<"Age       : "<<age<<endl;
+    cout<<"Percentage:"<<percentage<<endl;
+    cout<<"Grade     :"<<grade<<endl;
+    return 0;
+}
